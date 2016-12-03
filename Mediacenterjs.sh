812f6b01d850b5734a8b1dae049f831a1508467c
A-G-A <appana.gowtham@gmail.com>
2016-12-03 22:53:17 +0530

    ubuntu/trusty64

---
+++ Mediacenterjs.sh
@@ -0,0 +1,7 @@
+sudo apt-get install git # If you do not have git installed yet
+git clone https://github.com/A-G-A/mediacenterjs.git
+sudo apt-get install -y node
+sudo apt-get install -y nodejs-legacy
+cd mediacenterjs
+npm install
+npm start 
